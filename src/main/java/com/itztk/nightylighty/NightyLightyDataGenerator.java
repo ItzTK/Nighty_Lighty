a272ede75a652a21f19689afa1984fc24caa93b0
ItzTK <tylergordineer@gmail.com>
2025-03-21 15:37:16 -0700

    Initial commit

---
+++ src/main/java/com/itztk/nightylighty/NightyLightyDataGenerator.java
@@ -0,0 +1,11 @@
+package com.itztk.nightylighty;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class NightyLightyDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
